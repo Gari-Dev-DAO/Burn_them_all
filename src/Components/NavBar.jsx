f98adf7f351540de9106907ad9426d9fd8aa224b
ravikant333 <ravikant@chingari.io>
2023-01-11 23:25:19 +0530

    styles updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,19 +1,28 @@
+
+import { TfiAlignJustify } from "react-icons/tfi";
 import ConnetWalletButton from "./ConnetWalletButton"
+import Button from "./Button";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isToggle,setIsToggle]=useState(false)
   return (
     <div className='navBar'>
+       <div className="toggleBar"><Button onClick={()=>setIsToggle(!isToggle)}><TfiAlignJustify size='32' color='white'/></Button></div>
+       <div  className={`nav ${!isToggle ? "hide" : ""}`}>
         <div className="navLinkBox">
          <a className="link">Go bonk Errs</a>
         </div>
-        <div  className="navLinkBox" >
+        <div  className="navLinkBox navlinks" >
         <a href="https://www.coingecko.com/en/coins/bonk" target='_blank' className="link">Buy Bonk</a>
          <a href="https://web.telegram.org/k/#-813184380" target='_blank' className="link">Telegram</a>
         </div>
         <div className="navLinkBox">
      <ConnetWalletButton/>
         </div>
+        </div>
     </div>
+   
   )
 }
 
