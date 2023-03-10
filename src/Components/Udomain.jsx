aa6dd6c8400e58cbc16bdbec236ab8f19667c9b1
Ravikant Mishra <118275688+ravikant333@users.noreply.github.com>
2023-03-10 13:57:20 +0530

    Update Udomain.jsx

--- src/Components/Udomain.jsx
+++ src/Components/Udomain.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import UAuth from "@uauth/js";
+import React, { useState } from "react"
+import UAuth from "@uauth/js"
+
 
 const uauth = new UAuth({
-  clientID: "f9c1c68d-6191-4cb3-b52a-1ff774e2926b",
-  redirectUri: "https://burn-all.vercel.app/", 
-  scope: "openid wallet email profile:optional social:optional",
-});
+  clientID: "331542d7-ddd5-4444-b380-3b4b4cf38540",
+  redirectUri: "https://burn-all.vercel.app/",
+  scope: "openid wallet email profile:optional social:optional"
+})
 
 function UDomain() {
-  const [Uauth, setUauth] = useState();
+    const [Uauth, setUauth] = useState()
 
-  async function Connect() {
-    try {
-      const authorization = await uauth.loginWithPopup();
-      setUauth(JSON.parse(JSON.stringify(authorization))["idToken"]);
+    async function Connect() {
+        try {
+            const authorization = await uauth.loginWithPopup()
+            setUauth(JSON.parse(JSON.stringify(authorization))["idToken"])
 
-      await authorization();
-    } catch (error) {
-      console.error(error);
+            // await authenticate()
+            await authorization();
+        } catch (error) {
+            console.error(error)
+        }
     }
-  }
 
-  async function logOut() {
-    await uauth.logout();
-    setUauth(null);
-  }
-
-  function log() {
-    if (Uauth === null || Uauth === undefined) {
-      Connect();
-    } else {
-      logOut();
+    async function logOut() {
+       await uauth.logout()
+       setUauth(null)
     }
-  }
 
-  return (
-    <>
-      <button className="walletBtn" onClick={log}>
-        {Uauth != null ? Uauth["sub"] : "LogIn (UNSD)"}
-      </button>
-    </>
-  );
+    function log() {
+        if (Uauth === null || Uauth === undefined) {
+            Connect()
+        } else {
+            logOut()
+        }
+    }
+
+    return (
+        <>
+            <button className="walletBtn" onClick={log}>{Uauth != null ? Uauth["sub"] : "Login with UNSD"}</button>
+        </>
+    )
 }
 
-export default UDomain;
+export default UDomain
